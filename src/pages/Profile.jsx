ab9fe7f8d4480b78d20c2886600b5d70c148ac2e
Alan Liang <alanliang905@gmail.com>
2024-07-27 18:16:00 -0500

    remove config

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,18 +1,35 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import {useParams } from 'react-router-dom';
-import Context from '../navigation/context';
+import LoadingIcon from '../assets/LoadingIcon';
 
 export default function Profile() {
     const { userId } = useParams();
-    const { user } = useContext(Context);
+    const [data, setData] = useState(null);
+    const navigate = useNavigate();
 
     useEffect(() => {
         console.log(userId);
     }, [userId]);
 
+    const [profilePictureUrl, setProfilePictureUrl] = useState(defaultPFP);
+
+    useEffect(() => { //search yser
+        const fetchProfilePicture = async () => {
+            const docRef = doc(db, "users", userId);
+            const docSnap = await getDoc(docRef);
+            if (docSnap.exists()) {
+                setData(docSnap.data());
+
+            } else {
+                navigate('/404');
+            }
+        };
+        
+    }, []);
+
     return <div>
         <h1>Profile</h1>
-        <h2>{user.displayName}</h2>
         <h3>User ID: {userId}</h3>
     </div>;
 }
